28f7e965b727f5cb8c5078fd41aece82bb806bad
MokhammadVu <stechkin_95@hotmail.com>
2023-07-24 14:15:34 +0200

    first commit

---
+++ calculator/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+   setResult(result.concat(e.target.name));
+  }
+
+  const clear = () => {
+    setResult("");
+  }
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  }
+  
+  const procentCalculate = () => {
+    try {
+      const resultProcent = eval(result) / 100;
+      setResult(resultProcent.toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  }
+
+  return (
+    <>
+      <div className='container'>
+          <form>
+              <input placeholder='Calculator' type="text" value={result} readOnly/>
+          </form>
+
+          <div className="keypad">
+            <button className='clear' onClick={clear}>Clear</button>
+            <button className='keypad_orange' onClick={procentCalculate} name="%">%</button>
+            <button className='keypad_orange' name="/" onClick={handleClick}>&divide;</button>
+            <button  name="7" onClick={handleClick}>7</button>
+            <button  name="8" onClick={handleClick}>8</button>
+            <button  name="9" onClick={handleClick}>9</button>
+            <button className='keypad_orange' name="*" onClick={handleClick}>&times;</button>
+            <button  name="4" onClick={handleClick}>4</button>
+            <button  name="5" onClick={handleClick}>5</button>
+            <button  name="6" onClick={handleClick}>6</button>
+            <button className='keypad_orange' name="-" onClick={handleClick}>&ndash;</button>
+            <button  name="1" onClick={handleClick}>1</button>
+            <button  name="2" onClick={handleClick}>2</button>
+            <button  name="3" onClick={handleClick}>3</button>
+            <button  className='keypad_orange'  name="+" onClick={handleClick}> +</button>
+            <button  className='zero' name="0" onClick={handleClick}>0</button>
+            <button  name="." onClick={handleClick}>.</button>
+            <button  className='keypad_orange' onClick={calculate}>=</button>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
